5ce84e5b7e011b693dc441630627eb1cbb566ecd
Aleksandr Chudak <63600579+AlexChudak@users.noreply.github.com>
2021-03-18 13:41:19 +0200

    Fix Commit

--- foods/src/localStorage.js
+++ foods/src/localStorage.js
@@ -1,5 +1,6 @@
 const refs = {
-  body: document.querySelector('body'),
+  // body: document.querySelector('body'),
+  body: document.body,
   switch: document.querySelector('.js-switch-input'),
 };
 
@@ -8,31 +9,40 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
+//По дефолту светлая тема
+refs.body.classList.add(Theme.LIGHT);
+
 refs.switch.addEventListener('change', setClassList);
-refs.switch.addEventListener('change', setLocalStorage);
+// refs.switch.addEventListener('change', setLocalStorage);
 
 function setClassList(e) {
+  refs.body.classList.add(Theme.LIGHT);
   const check = refs.switch.checked;
 
   if (check) {
     refs.body.classList.add(Theme.DARK);
     refs.body.classList.remove(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
   } else {
     refs.body.classList.add(Theme.LIGHT);
     refs.body.classList.remove(Theme.DARK);
-  }
-}
-
-function setLocalStorage(e) {
-  const check = refs.switch.checked;
-
-  if (check) {
-    localStorage.setItem('theme', Theme.DARK);
-  } else {
-    localStorage.removeItem('theme');
+    // localStorage.removeItem('theme');
     localStorage.setItem('theme', Theme.LIGHT);
   }
 }
+
+
+
+// function setLocalStorage(e) {
+//   const check = refs.switch.checked;
+
+//   if (check) {
+//     localStorage.setItem('theme', Theme.DARK);
+//   } else {
+//     localStorage.removeItem('theme');
+//     localStorage.setItem('theme', Theme.LIGHT);
+//   }
+// }
 
 const themeInLocal = localStorage.getItem('theme');
 
@@ -40,3 +50,4 @@ if (themeInLocal === Theme.DARK) {
   refs.body.classList.add(Theme.DARK);
   refs.switch.checked = true;
 }
+else (themeInLocal === Theme.LIGHT) 
